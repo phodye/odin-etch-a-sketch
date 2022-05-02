e7fa440e98f9a649df8421db0a9920025634f39e
Andrew Dye <phodye@gmail.com>
2022-05-02 15:36:16 -0400

    Created the four by four square of divs

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('#container');
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "50px";
+
+for (x = 0; x < 16; x++) {
+    const div = document.createElement('div');
+    div.style.border = "thin solid #000000";
+    div.style.height = "10px";
+    div.style.width = "10px";
+    container.appendChild(div);
+}
+

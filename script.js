11c82a5fbbc45f66dcf67ba3d60dcd73106bbaf2
Andrew Dye <phodye@gmail.com>
2022-05-02 16:57:46 -0400

    Self generating grid

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+let gridSize = 8;
+let numberofDivs = gridSize * gridSize;
+let divDimension = 700 / gridSize;
+console.log(divDimension);
+
 const container = document.querySelector('#container');
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
-container.style.width = "50px";
+container.style.width = "700px";
+container.style.height = "700px";
 
-for (x = 0; x < 16; x++) {
+for (x = 0; x < numberofDivs; x++) {
     const div = document.createElement('div');
+    div.style.boxSizing = "border-box";
     div.style.border = "thin solid #000000";
-    div.style.height = "10px";
-    div.style.width = "10px";
+    div.style.height = `${divDimension}px`;
+    div.style.width = `${divDimension}px`;
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = "purple";
     }) 

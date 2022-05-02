343bed9beaf08d6c084c74271ee1498cc98be8df
Andrew Dye <phodye@gmail.com>
2022-05-02 17:39:26 -0400

    Accepts input to create grid

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let gridSize = 8;
-let numberofDivs = gridSize * gridSize;
-let divDimension = 700 / gridSize;
-console.log(divDimension);
+//get grid size
+const body = document.querySelector("body");
+const section = document.querySelector("section");
 
+const gridInput = document.createElement('input');
+gridInput.setAttribute("type", "text");
+gridInput.setAttribute("value", "Input Grid Size") 
+gridInput.style.margin = "10px"; 
+gridInput.style.rightMargin = "2px";
+body.insertBefore(gridInput, section);
+
+console.log(gridInput.value);
+
+const pressMe = document.createElement("button");
+pressMe.textContent = "Create Grid";
+body.insertBefore(pressMe, section);
+pressMe.addEventListener('click', () => buildGrid(gridInput.value));
+
+//create grid 
 const container = document.querySelector('#container');
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
-container.style.width = "700px";
-container.style.height = "700px";
+container.style.width = "600px";
+container.style.height = "600px";
+
+function buildGrid(input) {
+const gridSize = input;
+let numberofDivs = gridSize * gridSize;
+let divDimension = 600 / gridSize;
+console.log(input);
 
 for (x = 0; x < numberofDivs; x++) {
     const div = document.createElement('div');
@@ -16,9 +36,10 @@ for (x = 0; x < numberofDivs; x++) {
     div.style.height = `${divDimension}px`;
     div.style.width = `${divDimension}px`;
     div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "purple";
+        div.style.backgroundColor = "black";
     }) 
     container.appendChild(div);
+}
 }
 
 

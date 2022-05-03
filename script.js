b7e987a1b6d3df5d23e657144c4f49802c3cd9aa
Andrew Dye <phodye@gmail.com>
2022-05-03 16:02:09 -0400

    Added random color option for squares

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//create grid 
+//create grid- container 
 const container = document.querySelector('#container');
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
 container.style.width = "600px";
 container.style.height = "600px";
 
+const lowerContiner = document.querySelector('#lowerContainer');
+
+//create grid- build function
 function buildGrid(input) {
 container.innerHTML = "";
 
@@ -16,23 +19,50 @@ console.log(input);
 for (x = 0; x < numberofDivs; x++) {
     const div = document.createElement('div');
     div.style.boxSizing = "border-box";
-    div.style.border = "thin solid #000000";
+    div.style.border = ".5px solid #454545";
     div.style.height = `${divDimension}px`;
     div.style.width = `${divDimension}px`;
     div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "black";
+        if (colorChooser.textContent === "Black and White") {
+                div.style.backgroundColor = "black";
+        } else if (colorChooser.textContent === "Color Mix") {
+                div.style.backgroundColor = getRandomColor();
+        }
     }) 
     container.appendChild(div);
 }
 }
 
-// Slider
+//slider for grid size
 let slider = document.getElementById("myRange");
 let output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
+//Updates the output value and invokes buildGrid when slider is updated
 slider.oninput = function() {
     output.innerHTML = this.value;
     buildGrid(this.value);
   } 
+
+//color/bw button
+let colorChooser = document.createElement('button');
+colorChooser.textContent = "Black and White";
+colorChooser.addEventListener('click', () => {
+    if (colorChooser.textContent === "Black and White") {
+        colorChooser.textContent = "Color Mix"
+    } else if (colorChooser.textContent === "Color Mix") {
+        colorChooser.textContent = "Black and White"
+    }
+    console.log(colorChooser.textContent)
+})
+lowerContainer.appendChild(colorChooser);
+
+//random color generator 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
